9638af44526a0e38c0a7769c5b48f3bd3d7473c7
Joe Balzac <jgbalzac@gmail.com>
2024-12-30 00:28:08 -0500

    adds current weather

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,8 +1,20 @@
+import { Card, Image, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
+import { FaCloud } from "react-icons/fa";
 
 interface WeatherData {
+  location: {
+    name: string;
+    country: string;
+    region: string;
+  };
+
   current: {
     temperature: number;
+    weather_descriptions: string[];
+    observation_time: string;
+    weather_icons: string[];
+    feelslike: number;
   };
 }
 
@@ -17,9 +29,25 @@ const WeatherCard = () => {
   }
 
   return (
-    <div>
-      <h1>{data?.current.temperature}</h1>
-    </div>
+    <Card.Root width="320px">
+      <Card.Body gap="2">
+        <FaCloud />
+        <Card.Title mt="2">
+          {data?.location.name} {data?.location.country},{" "}
+          {data?.location.region}
+        </Card.Title>
+        <Card.Description>
+          {data?.current.weather_descriptions[0]}
+          <Text as="span" fontWeight="bold">
+            {" "}
+            Feels like: {data?.current.feelslike}°C
+          </Text>
+        </Card.Description>
+      </Card.Body>
+      <Card.Footer justifyContent="flex-end">
+        <Card.Description>{data?.current.temperature}°C</Card.Description>
+      </Card.Footer>
+    </Card.Root>
   );
 };
 
