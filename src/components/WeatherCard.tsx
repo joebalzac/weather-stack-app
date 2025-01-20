b409aa3ddcf697d4a29411ccc272ae3429e98417
Joe Balzac <joebalzac@Mac.lan>
2025-01-19 23:15:51 -0500

    adds hourly and current toggle

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,8 +1,15 @@
-import { Card, Flex, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import { FaCloud } from "react-icons/fa";
 import useLocation from "../hooks/useLocation";
-import useGeoCoding from "@/hooks/useGeoCoding";
+import useGeoCoding from "../hooks/useGeoCoding";
+import { useState } from "react";
+
+interface HourlyWeather {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  weather: { main: string; description: string; icon: string };
+}
 
 interface WeatherData {
   current: {
@@ -18,7 +25,12 @@ interface WeatherData {
       description: string;
     };
   };
+  hourly: HourlyWeather[];
 }
+
+const kelvinToFahrenheit = (kelvin: number): string => {
+  return `${(((kelvin - 273.15) * 9) / 5 + 32).toFixed(2)}°F`;
+};
 
 const WeatherCard = () => {
   const newYorkLocation = { lat: 40.7282, lon: -73.7949 };
@@ -36,6 +48,13 @@ const WeatherCard = () => {
     },
   });
 
+  const [showHourly, setShowHourly] = useState(false);
+
+  const handleSelectedHourly = () => {
+    setShowHourly(!showHourly);
+    console.log("hello motherfucker");
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -50,30 +69,56 @@ const WeatherCard = () => {
   }
 
   return (
-    <Card.Root className="text-gray-50 border-4 border-gray-100 shadow-lg p-8 bg-blue-950 rounded-md ">
-      <Card.Body gap="2">
-        <FaCloud />
-        <Card.Title mt="2">
-          {locationName ? locationName : "Unknown location"}
-        </Card.Title>
-      </Card.Body>
-      <div className="flex justify-between items-end">
-        <Flex direction={"column"}>
-          <Text className="text-gray-50">
-            {data?.current.weather.description}
-          </Text>
-          <Text as="span" fontWeight="bold" className="text-gray-50">
-            Feels like: {data?.current.feels_like}°C
-          </Text>
-        </Flex>
-        <Flex direction={"column"}>
-          <Text>Wind: {data?.current.wind_speed} kmph</Text>
-
-          <Text>Pressure: {data?.current.pressure} mb</Text>
-          <Text className="text-3xl">{data?.current.temp}°C</Text>
-        </Flex>
-      </div>
-    </Card.Root>
+    <div className="flex flex-col justify-between items-center text-gray-50 border-4 border-gray-100 shadow-lg p-8 bg-blue-400 rounded-md w-full">
+      <button
+        className=" bg-transparent text-white text-2xl border-teal-300 py-1 px-4"
+        onClick={handleSelectedHourly}
+      >
+        {showHourly ? "Show Current" : "Show Hourly"}
+      </button>
+      {showHourly ? (
+        <div>
+          <h2>Hourly</h2>
+          <div className="flex gap-4">
+            {data?.hourly.slice(0, 6).map((hour) => (
+              <div>
+                <div className="flex flex-col">
+                  {new Date(hour.dt * 1000).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                  <p>{kelvinToFahrenheit(hour.temp)}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="pb-6">
+            <FaCloud />
+            <h1 className="text-6xl pb-4">
+              {locationName ? locationName : "Unknown location"}
+            </h1>
+            <h2 className="text-5xl">
+              {kelvinToFahrenheit(data?.current.temp ?? 0)}
+            </h2>
+            <h3 className="text-1xl text-gray-50 font-semibold py-2">
+              Feels like: {kelvinToFahrenheit(data?.current.feels_like ?? 0)}
+            </h3>
+          </div>
+          <div className="flex justify-between items-end">
+            <div className="flex flex-col">
+              <h4 className="text-gray-50">
+                {data?.current.weather.description}
+              </h4>
+              <h4>Wind: {data?.current.wind_speed} kmph</h4>
+              <h4>Pressure: {data?.current.pressure} mb</h4>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,42 +1,29 @@
-import { AxiosRequestConfig, CanceledError } from "axios";
+import { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-const useData = <T>(
-  endpoint: string,
-  requestConfig?: AxiosRequestConfig,
-  deps?: any[]
-) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig) => {
   const [data, setData] = useState<T>();
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(
-    () => {
-      
-      const controller = new AbortController();
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const response = await apiClient.get<T>(endpoint, {
-            signal: controller.signal,
-            ...requestConfig,
-          });
-          setData(response.data);
-        } catch (err: unknown) {
-          if (err instanceof CanceledError) return;
-          setError(
-            err instanceof Error ? err.message : "An unknown error occurred"
-          );
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchData();
-      return () => controller.abort();
-    },
-    deps ? [...deps] : []
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await apiClient.get<T>(endpoint, {
+          ...requestConfig,
+        });
+        setData(response.data);
+      } catch (error) {
+        if (error instanceof Error) return;
+        setError("An unknown error occurred");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   return { data, error, isLoading };
 };
